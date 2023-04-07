aade4d54ec9b0d84b94cc3d33302bced3344dbda
LolaInTa Chi <lolainta@gmail.com>
2023-04-08 00:14:38 +0800

    feat: generate from all scene

--- src/main.py
+++ src/main.py
@@ -6,12 +6,15 @@ from NuscData import NuscData
 
 def main():
     nusc = NuScenes(version='v1.0-mini', dataroot='./data', verbose=False)
-    nuscData: NuscData = NuscData(nusc, 5)
-    gen: Generator = Generator(nuscData)
-    plt: Drawer = Drawer()
-    dataCluster = gen.gen_all()
-    for dataSet in dataCluster:
-        plt.plot_dataset(dataSet, atk=True)
+    for i in range(len(nusc.scene)):
+        nuscData: NuscData = NuscData(nusc, i)
+        gen: Generator = Generator(nuscData)
+        dataCluster = gen.gen_all()
+        print(f'{len(dataCluster)} of data generated in scene[{i}]')
+        # for dataset in dataCluster:
+        #     plt: Drawer = Drawer()
+        #     plt.plot_dataset(dataset)
+        #     plt.plot_dataset(dataset, atk=True)
 
 
 if __name__ == '__main__':
