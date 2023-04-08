075022510b3c2b764e0c424328f5fdf44a0e7f77
LolaInTa Chi <lolainta@gmail.com>
2023-04-08 10:38:20 +0800

    feat: arg parser

--- src/main.py
+++ src/main.py
@@ -3,20 +3,54 @@ from Generator import Generator
 from NuscData import NuscData
 import pickle
 import os
+import argparse
 
 
-def main():
-    nusc = NuScenes(version='v1.0-mini', dataroot='./data', verbose=False)
+def run(args):
+    nusc = NuScenes(version=args.dataset, dataroot='./data', verbose=True)
+    cnt = 0
+    print("Generating data...")
     for i in range(len(nusc.scene)):
+        # print(f'Generate data for scene {nusc.scene[i]["token"]}')
         nuscData: NuscData = NuscData(nusc, i)
         gen: Generator = Generator(nuscData)
         dataCluster = gen.gen_all()
-        print(f'{len(dataCluster)} of data generated in scene[{i}]')
-        scene_dir = os.path.join('./records/', dataCluster[0].scene['token'])
-        os.makedirs(scene_dir, exist_ok=True)
-        for dataset in dataCluster:
-            with open(os.path.join(scene_dir, f'{dataset.inst["token"]}.pikle'), 'wb') as f:
-                pickle.dump(dataset, f, protocol=pickle.HIGHEST_PROTOCOL)
+        sz = len(dataCluster)
+        cnt += sz
+        print(f'{sz} data generated in scene[{i}]')
+        if sz == 0:
+            print(f"No data generated for this scene, skip")
+            continue
+        if args.record:
+            scene_dir = os.path.join(
+                args.record_path, dataCluster[0].scene['token'])
+            os.makedirs(scene_dir, exist_ok=True)
+            for dataset in dataCluster:
+                with open(os.path.join(scene_dir, f'{dataset.inst["token"]}.pickle'), 'wb') as f:
+                    pickle.dump(dataset, f, protocol=pickle.HIGHEST_PROTOCOL)
+    print('======')
+    print(f'{cnt} data generated from {len(nusc.scene)} scenes')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='python3 src/main.py',
+        description='Generate data from given nuscene dataset and collision type'
+    )
+    parser.add_argument('-d', '--dataset',
+                        choices=['v1.0-mini', 'v1.0-trainval'],
+                        default='v1.0-mini',
+                        help='Nuscene dataset version')
+    parser.add_argument('--record',
+                        action='store_true',
+                        default=False,
+                        help='Whether to record to record path or not)')
+    parser.add_argument('--record-path',
+                        default='./records',
+                        help='The path to store the generated data (create if not exists)'
+                        )
+    args = parser.parse_args()
+    run(args)
 
 
 if __name__ == '__main__':
--- src/visualize.py
+++ src/visualize.py
@@ -1,14 +1,23 @@
 import pickle
 import sys
+import os
+import random
 from Drawer import Drawer
 
 
 def main():
     if len(sys.argv) == 1:
-        print(f'Usage: python3 {sys.argv[0]} <piklefile>')
-        exit(1)
-    print(f'Loading file: {sys.argv[1]}')
-    with open(sys.argv[1], 'rb') as f:
+        print(f'Usage: python3 {sys.argv[0]} <picklefile>')
+        print(f'No argument given, randomly pick one from ./records')
+        scenes = os.listdir('./records')
+        scene = random.sample(scenes, 1)[0]
+        insts = os.listdir(os.path.join('./records', scene))
+        inst = random.sample(insts, 1)[0]
+        record = os.path.join('./records', scene, inst)
+    else:
+        record = sys.argv[1]
+    print(f'Loading file: {record}')
+    with open(record, 'rb') as f:
         dataset = pickle.load(f)
     plt = Drawer()
     plt.plot_dataset(dataset)
